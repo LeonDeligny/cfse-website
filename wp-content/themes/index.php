66444757c54ba2ff4ace34e4bc7d8a43fab52294
LeonDeligny <leon.deligny@icloud.com>
2025-09-18 10:40:25 +0200

    add plugin

---
+++ wp-content/themes/index.php
@@ -0,0 +1,15 @@
+<?php
+<?php
+/**
+ * Minimal index for cfse-website theme.
+ */
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+get_header();
+?>
+<main id="site-content">
+    <h1><?php bloginfo( 'name' ); ?></h1>
+    <p><?php bloginfo( 'description' ); ?></p>
+</main>
+<?php
+get_footer();
